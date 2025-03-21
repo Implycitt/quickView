9eb38b15720efefd3ec823ae9470c96dc687e2c3
Implycitt <qgbordelon@gmail.com>
2025-03-20 22:55:32 -0500

    a little more structure

--- src/.electron/electron.js
+++ src/.electron/electron.js
@@ -1,22 +1,27 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
-  })
+    height: 600,
+    title: "quickView",
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
 
-  win.loadFile('index.html')
+  win.loadFile('./public/index.html');
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
   app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
   })
-})
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
-})
+});
